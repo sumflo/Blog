a6ee5d6a963c66c3a73855ff164f98fbb7758c10
sumflo <real61madrid@gmail.com>
2021-10-12 14:09:36 +0200

    update

--- src/main/java/homework/maven/springframework/blog/security/WebSecurity.java
+++ src/main/java/homework/maven/springframework/blog/security/WebSecurity.java
@@ -1,5 +1,9 @@
 package homework.maven.springframework.blog.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +13,31 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+   /* @Override
     protected void configure(HttpSecurity http) throws Exception{
 
-        CharacterEncodingFilter filter = new
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login");
+
+        *//*CharacterEncodingFilter filter = new
                 CharacterEncodingFilter();
         http.addFilterBefore(filter, CsrfFilter.class)
                 .formLogin()
@@ -24,6 +49,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/loginpage/beforelogin").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated();*//*
     }
+*/
 }
