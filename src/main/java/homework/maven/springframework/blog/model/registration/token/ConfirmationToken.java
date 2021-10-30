c9a53b736f91b23373961cf5482a59a84124a45d
sumflo <real61madrid@gmail.com>
2021-10-30 17:42:19 +0200

    update

--- src/main/java/homework/maven/springframework/blog/model/registration/token/ConfirmationToken.java
+++ src/main/java/homework/maven/springframework/blog/model/registration/token/ConfirmationToken.java
@@ -1,10 +1,13 @@
 package homework.maven.springframework.blog.model.registration.token;
 
+import homework.maven.springframework.blog.model.User;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,21 +20,31 @@ import lombok.Setter;
 public class ConfirmationToken {
 
   @Id
-  @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+  @SequenceGenerator(name = "confirmation_token_sequence", sequenceName = "confirmation_token_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "confirmation_token_sequence")
   private Long id;
 
   @Column(nullable = false)
   private String token;
+
+  @Column(nullable = false)
   private LocalDateTime createdAt;
-  private LocalDateTime expiredAt;
+
+  @Column(nullable = false)
+  private LocalDateTime expiresAt;
+
   private LocalDateTime confirmedAt;
 
-  public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiredAt,
-      LocalDateTime confirmedAt) {
+  @ManyToOne
+  @JoinColumn(nullable = false, name = "user_id")
+  private User user;
+
+  public ConfirmationToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt,
+      LocalDateTime confirmedAt, User user) {
     this.token = token;
     this.createdAt = createdAt;
-    this.expiredAt = expiredAt;
+    this.expiresAt = expiresAt;
     this.confirmedAt = confirmedAt;
+    this.user = user;
   }
 }
